225f4b0ac85ba0f5af2ddfa06d34d710e2a24bb2
Logan Wright <logan.william.wright@gmail.com>
2016-06-09 22:58:24 -0400

    basic scaffolding

--- Package.swift
+++ Package.swift
@@ -3,15 +3,14 @@ import PackageDescription
 let package = Package(
     name: "VaporApp",
     dependencies: [
-        .Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0, minor: 10),
-		.Package(url: "https://github.com/qutheory/vapor-mustache.git", majorVersion: 0, minor: 6)
+        .Package(url: "https://github.com/loganwright/vapor.git", majorVersion: 0, minor: 42),
     ],
     exclude: [
 	    "Config",
-        "Database",
-        "Localization",
-        "Public",
-        "Resources",
-		"Tests",
+      "Database",
+      "Localization",
+      "Public",
+      "Resources",
+		  "Tests",
     ]
 )
